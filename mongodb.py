efbcdfa0451c72abb3cb1ab8dee1f84bb98d22e4
Jay Kakadiya <jaykakadiya2014@gmail.com>
2022-07-20 12:34:21 +0530

    Create mongodb.py

---
+++ mongodb.py
@@ -0,0 +1,28 @@
+import motor.motor_asyncio
+
+MONGO_DETAILS = "" #server
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+database = client.data
+collection = database.get_collection("movies")
+
+print(database)
+print(collection.find_one())
+
+async def do_count():
+    n = await collection.count_documents({})
+    print('%s documents in collection' % n)
+
+loop = client.get_io_loop()
+loop.run_until_complete(do_count()) #for mongodb motor
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+import pymongo
+
+myclient = pymongo.MongoClient("") #server
+mydb = myclient["data"]
+mycol = mydb["movies"]
+
+myquery = { 'name':"Psycho" }
+for x in mycol.find().sort("name",-1):
+  print(x) #simple mongodb
