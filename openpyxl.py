11b2395e31393b12ddee7ec13a4c5aed7eac684c
Jay Kakadiya <jaykakadiya2014@gmail.com>
2022-07-20 12:36:18 +0530

    Update openpyxl.py

--- openpyxl.py
+++ openpyxl.py
@@ -1,12 +1,14 @@
 from openpyxl import load_workbook
 
-wb = load_workbook(r'C:\test\test.xlsx')
+wb = load_workbook(r'C:\test\test.xlsx') #load excel
 sheet = wb.active
 max_row = sheet.max_row
 print(max_row)
 
 sheet['A1'] = 'School name'
 sheet['B1'] = 'school url'
-sheet.cell(row=max_row + 1, column=4).value = 5
+sheet.cell(row=max_row + 1, column=4).value = 5 #add value in cell
 
-wb.save(r'C:\test\test.xlsx')
+sheet.cell(row=1, column=2).value = "Hi" #add value in cell
+
+wb.save(r'C:\test\test.xlsx') #save excel
