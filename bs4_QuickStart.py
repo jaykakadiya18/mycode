2cef5278341aa625974a9ea980fcf18ba9c4307c
Jay Kakadiya <jaykakadiya2014@gmail.com>
2022-08-04 18:29:14 +0530

    Update bs4_QuickStart.py

--- bs4_QuickStart.py
+++ bs4_QuickStart.py
@@ -32,4 +32,5 @@ from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+# driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
