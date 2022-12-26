67568d1309381483942ea87f9f08219d584dfb55
Jay Kakadiya <jaykakadiya2014@gmail.com>
2022-12-26 22:58:04 +0530

    csv added

--- openpyxl.py
+++ openpyxl.py
@@ -12,3 +12,20 @@ sheet.cell(row=max_row + 1, column=4).value = 5 #add value in cell
 sheet.cell(row=1, column=2).value = "Hi" #add value in cell
 
 wb.save(r'C:\test\test.xlsx') #save excel
+
+#------------------CSV--------------------------------------
+
+import csv
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    with open('new_names.csv', 'w') as new_file:
+        fieldnames = ['first_name', 'last_name'] #optional
+
+        csv_writer = csv.writer(new_file, fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader() #optional
+
+        for line in csv_reader:
+            csv_writer.writerow(line[3])  #row 3
